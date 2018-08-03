3f2ed16f36c04ec2cab2cddf109d3baf3660e324
Ayushi Negi <ayushin78@gmail.com>
2018-08-03 12:50:01 +0530

    Add person.go

--- deck.go
+++ deck.go
@@ -1,11 +1,11 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"math/rand"
 	"os"
-	"fmt" 
 	"strings"
-	"io/ioutil"
 )
 
 type deck []string
@@ -16,37 +16,37 @@ func (d deck) print() {
 	}
 }
 
-func (d deck) toString() (string) {
+func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
 }
 
-func deal (d deck, handsize int) (deck, deck){
-	return d[:handsize],d[handsize:] 
+func deal(d deck, handsize int) (deck, deck) {
+	return d[:handsize], d[handsize:]
 }
 
-func newDeck() deck { 
+func newDeck() deck {
 	cards := deck{}
 	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
-	cardValues := []string{"Ace", "Two","Three", "Four"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, value+" of "+ suit)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
-	
+
 	return cards
 }
 
-func (d deck) saveDeckToFile(fileName string) error{
-	return ioutil.WriteFile(fileName, []byte(d.toString()), 777) 
+func (d deck) saveDeckToFile(fileName string) error {
+	return ioutil.WriteFile(fileName, []byte(d.toString()), 777)
 }
 
 func newDeckFromFile(fileName string) deck {
 	bs, err := ioutil.ReadFile(fileName)
-	if(err != nil){
+	if err != nil {
 		fmt.Println("error : ", err)
-		os.Exit(1);
+		os.Exit(1)
 	}
 	s := strings.Split(string(bs), ",")
 	return deck(s)
@@ -56,5 +56,12 @@ func (d deck) shuffle() {
 	for i := range d {
 		j := rand.Intn(len(d))
 		d[i], d[j] = d[j], d[i]
+	}
+
+}
+
+func (d deck) update() {
+	for i := range d {
+		d[i] = d[0]
 	}
 }
--- main.go
+++ main.go
@@ -1,8 +1,19 @@
 package main
 
-func main(){
-	cards := newDeckFromFile("my_file")
-	cards.print()
-	cards.shuffle()
-}
 
+func main() {
+	// cards := newDeck()
+	// cards.print()
+	// cards.update()
+	// fmt.Println("------------------------")
+	// cards.print()
+
+	p := person{
+		firstName : "Ayushi",
+		lastName : "Negi",
+	}
+
+	p.print()
+	p.updateFirstName("shikha")
+	p.print()
+}
